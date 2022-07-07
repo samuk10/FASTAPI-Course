17f27296f151bef711f7a52994db78dd863630ab
samuk10 <samukasampaio@hotmail.com>
2022-07-07 20:02:17 -0300

    create, delete and update with ORM using sessions

--- app/main.py
+++ app/main.py
@@ -86,21 +86,33 @@ def get_posts(db: Session = Depends(get_db)):
 
 #-- CREATE --#
 @app.post("/posts", status_code=status.HTTP_201_CREATED) # alterado para 201 ao criar
-def create_posts(post: Post):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, 
-    (post.title, post.content, post.published)) # não da pra passar o valor direto no insert
-                    # NÃO USAR (f"INSERT etc...) perigo de sql injection
-    new_post = cursor.fetchone() # mostra o retorno do insert no sql
+def create_posts(post: Post, db: Session = Depends(get_db)):
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, 
+    # (post.title, post.content, post.published)) # não da pra passar o valor direto no insert
+    #                 # NÃO USAR (f"INSERT etc...) perigo de sql injection
+    # new_post = cursor.fetchone() # mostra o retorno do insert no sql
+ 
+    # conn.commit()
 
-    conn.commit()
+    # enves de ter que digitar toda vez:
+    #new_post = models.Post(title=post.title, content=post.content, published=post.published)
+
+    #usar para criar dict e retornar todos fields model
+    new_post = models.Post(**post.dict())
+    # não esquecer de add e comitar
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post) # pega o que foi adicionado, coloca na variável e retorna
 
     return {"data": new_post} # send o dicionario em json
 
 #-- GET{ID} --#
 @app.get("/posts/{id}")
-def get_post(id: int): # converter para int (para eviter inject)
-    cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),)) # convert str pois dá erro no SELECT
-    post = cursor.fetchone()
+def get_post(id: int, db: Session = Depends(get_db)): # converter para int (para eviter inject)
+    #cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),)) # convert str pois dá erro no SELECT
+    #post = cursor.fetchone()
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with id: {id} was not found")
@@ -108,43 +120,59 @@ def get_post(id: int): # converter para int (para eviter inject)
         #return {'message': f"Post with id: {id} was not found"}
     return {"post_detail": post}
 
+
+
 #-- DELETE{ID} --# 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
+def delete_post(id: int, db: Session = Depends(get_db)):
     # deleting post
     # find the index in the array that has required ID
     # my_posts.pop(index)
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
-    deleted_post = cursor.fetchone()
-    conn.commit()
+    ## cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
+    ## deleted_post = cursor.fetchone()
+    ## conn.commit()
 
+    post = db.query(models.Post).filter(models.Post.id == id)
+    
     # se não existir o id =
-    if deleted_post == None:
+    if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
         detail=f"post with id: {id} does not exist")
+    # se existir o id = deletar ele.
+    post.delete(synchronize_session=False)    
+    db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
     #return {'message': 'post was succesfully deleted'}
 
 #-- UPDATE{ID} --# 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
+def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
 
-    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """,
-    (post.title, post.content, post.published, str(id)))
+    #cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """,
+    #(post.title, post.content, post.published, str(id)))
+    #updated_post = cursor.fetchone()
+    #conn.commit()
 
-    updated_post = cursor.fetchone()
-    conn.commit()
+    # procura um post
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    # pega o primeiro
+    post = post_query.first()
 
-    # se não existir o id =
-    if updated_post == None:
+    # se não existir o id da 404
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
         detail=f"post with id: {id} does not exist")
+
+    # passar o dict com os valores a serem atualizados
+    post_query.update(updated_post.dict(), synchronize_session=False)
+
+    db.commit()
 
     # Replace specific spot in the array
     # se existir o id , pega o dado em guardado em post converte par dict
 
-    return {'data': updated_post}
+    return {'data': post_query.first()}
 
 '''
 # modelo sem o BaseModel
