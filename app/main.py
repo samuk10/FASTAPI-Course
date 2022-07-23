544df0b547887940ade5f162a06e710623eb4d1d
samuk10 <samukasampaio@hotmail.com>
2022-07-22 22:45:57 -0300

    Setup CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
@@ -12,6 +14,18 @@ print(settings.database_username)
 #### models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+# function that runs before every request
+# by default only our domain can talk to the API
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # importing the routes from post.router
 app.include_router(post.router)
