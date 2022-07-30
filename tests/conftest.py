797477170909d9d1d869980310340e34981bcbe2
samuk10 <samukasampaio@hotmail.com>
2022-07-30 00:53:10 -0300

    finished tests, fixes postgres auth trust in compose

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,6 +13,7 @@ from app import models
 from alembic import command
 
 
+# 
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password123@localhost:5432/fastapi_test'
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
 
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -31,7 +31,7 @@ def test_multiply():
     assert multiply(4, 3) == 12
 
 
-def test_divide():
+def test_divde():
     assert divide(20, 5) == 4
 
 
@@ -60,13 +60,14 @@ def test_deposit(bank_account):
 def test_collect_interest(bank_account):
 
     bank_account.collect_interest()
-    assert round(bank_account.balance, 6) == 55 #round to 6 decimals
+    assert round(bank_account.balance, 6) == 55
 
 
 @pytest.mark.parametrize("deposited, withdrew, expected", [
     (200, 100, 100),
     (50, 10, 40),
     (1200, 200, 1000)
+
 ])
 def test_bank_transaction(zero_bank_account, deposited, withdrew, expected):
     zero_bank_account.deposit(deposited)
